c0c37d3334757426803b343ad3167f05d950e663
Kananek T <info.dvgamer@gmail.com>
2016-03-17 23:15:04 +0700

    update server

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 process.env['PATH'] = process.env['PATH']+';C:\\Program Files\\Git\\mingw64\\libexec\\git-core'
 
 var http = require('http');
-var repos = require('pushover')('D:/PGM-DevControl/tmp', { autoCreate: false });
-var config = require("app.config")[(/--(\w+)/.exec(process.argv[2] || '--serv') || ['', 'serv'])[1]]
-//
+var repos = require('pushover')(__dirname+'/tmp', { autoCreate: false });
+var config = require("./app.config");
+var conn = require("./libs/db");
 
-var permissableMethod = function(username, password, res) {
-  var user, _ref;
-  // this.log(username, 'is trying to', method, 'on repo:', repo.name, '...');
-  user = getUser(username, password, repo);
-  if (user === false) {
-    res.statusCode = 500;
-    // this.log(username, 'was rejected as this user doesnt exist, or password is wrong');
-    return res.end('Wrong username or password');
-  } else {
-    if (_ref = this.permMap[method], user.permissions.indexOf(_ref) >= 0) {
-      // this.log(username, 'Successfully did a', method, 'on', repo.name);
-      repos.handle(req, res); 
-    } else {
-    	res.statusCode = 500;
-      // this.log(username, 'was rejected, no permission to', method, 'on', repo.name);
-      return res.end("You dont have these permissions");
-    }
-  }
+var permissableMethod = function(username, password, req, res) {
+  repos.handle(req, res); 
+  // var user, _ref;
+  // // this.log(username, 'is trying to', method, 'on repo:', repo.name, '...');
+  // user = getUser(username, password, repo);
+  // if (user === false) {
+  //   res.statusCode = 500;
+  //   // this.log(username, 'was rejected as this user doesnt exist, or password is wrong');
+  //   return res.end('Wrong username or password');
+  // } else {
+  //   if (_ref = this.permMap[method], user.permissions.indexOf(_ref) >= 0) {
+  //     // this.log(username, 'Successfully did a', method, 'on', repo.name);
+  //     repos.handle(req, res); 
+  //   } else {
+  //   	res.statusCode = 500;
+  //     // this.log(username, 'was rejected, no permission to', method, 'on', repo.name);
+  //     return res.end("You dont have these permissions");
+  //   }
+  // }
 };
 
 var server = http.createServer(function (req, res) { 
@@ -35,27 +36,51 @@ var server = http.createServer(function (req, res) {
   } else {
     plain_auth = (new Buffer(auth.split(' ')[1], 'base64')).toString();
     creds = plain_auth.split(':');
-    permissableMethod(creds[0], creds[1], res);
+    permissableMethod(creds[0], creds[1], req, res);
   }
 });
 
 // EVENT
-
 repos.on('push', function (push) {
-    console.log('push ' + push.repo + '/' + push.commit + ' (' + push.branch + ')');
-    push.accept();
+  console.log('push ' + push.repo + '/' + push.commit + ' (' + push.branch + ')');
+  push.accept();
 });
 
 repos.on('fetch', function (fetch) {
-    console.log('fetch ' + fetch.repo + '/' + fetch.commit);
-    fetch.accept();
+  console.log('fetch ' + fetch.repo + '/' + fetch.commit);
+  fetch.accept();
 });
 
 repos.on('tag', function (tag) {
-    console.log('tag ' + tag.repo + '/' + tag.commit);
-    tag.accept();
+  console.log('tag ' + tag.repo + '/' + tag.commit);
+  tag.accept();
 });
 
+var db = conn.connect();
+db.select('url').then(function(rows){
+  rows.forEach(function(row){
+    var git = /\/(.*)\/(.*)/.exec(row.url);
+    repos.exists(row.url, function(found){
+      if(!found) {
+        repos.create(row.url, function(err){
+          console.log(err, row.url);
+        })
+      }
 
+    });
+    // repos.create(repoName, function(){
+
+    // })
+
+    // repos.mkdir(dir, function(){
+      
+    // })
+  });
+
+});
+// repos.exists('/dvgamers', function(found){
+//   console.log(found);
+
+// });
 console.log("Server listen on "+config.port);
 server.listen(config.port);
