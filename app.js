19b9a18d465047c1951c73fd809fc2a584ce72f9
KEM <kem@ns.co.th>
2016-03-17 11:05:15 +0700

    change engine to pushover

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-var GitServer = require('git-server');
-var newUser = {
-    username:'demo',
-    password:'demo'
-}
-var newRepo = {
-    name:'myrepo',
-    anonRead:true,
-    users: [
-        { user:newUser, permissions:['R','W'] }
-    ],
-    onSuccessful: {
-        fetch: function() {
-          return console.log('Successful fetch on "anon" repo');
-        },
-        push: function() {
-          return console.log('Success push on "anon" repo');
-        }
-    }
-}
+process.env['PATH'] = process.env['PATH']+';C:\\Program Files\\Git\\mingw64\\libexec\\git-core'
 
-server = new GitServer([ newRepo ], true, 'D:/tmp/repos', 7000);
-server.on('commit', function(update, repo) {
-    // do some logging or other stuff
-    update.accept() //accept the update.
+var pushover = require('pushover');
+var repos = pushover('/tmp/repos');
+
+repos.on('push', function (push) {
+    console.log('push ' + push.repo + '/' + push.commit
+        + ' (' + push.branch + ')'
+    );
+    push.reject("ignoe");
 });
-server.on('post-update', function(update, repo) {
-    //do some deploy stuff
+
+repos.on('fetch', function (fetch) {
+    console.log('fetch ' + fetch.repo + '/' + fetch.commit);
+    fetch.reject();
 });
+
+var http = require('http');
+var server = http.createServer(function (req, res) {
+    repos.handle(req, res);
+});
+server.listen(811);
