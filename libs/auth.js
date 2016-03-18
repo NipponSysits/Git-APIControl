62be2da9b61e53b2be5e59d207c954f188f92122
Kananek T <info.dvgamer@gmail.com>
2016-03-19 00:28:54 +0700

    auth update

--- libs/auth.js
+++ libs/auth.js
@@ -3,39 +3,51 @@ var conn = require("./db");
 var db = conn.connect();
 
 module.exports = {
-	authorization: function(headers) {
+	access: function(headers) {
     var def = Q.defer();
 	  var auth = headers['authorization'];
 	  if (auth === void 0) {
 	  	def.resolve(false);
 	  } else {
 	    var creds = (new Buffer(auth.split(' ')[1], 'base64')).toString().split(':');
-	    creds = { username: creds[0], password: creds[1] };
-	    db.selectOne('user', { username: creds.username }).then(function(row){
-	    	if(row) {
-	    		if(row.password !== creds.password) {
-	    			def.resolve(false);
-	    		} else {
-	    			db.selectOne('user_access', { user_id: row.user_id }).then(function(user){
-	    				def.resolve(user);
-	    			}).catch(function(ex){
-	    				console.log('user_access', ex);
-	    				def.resolve(false);
-	    			});
-	    		}
+	    var sql = 'SELECT password FROM user WHERE username=:username';
+	    db.query(sql, { username: creds[0] }).then(function(row){
+	    	if(row.length > 0) {
+	    		def.resolve(row[0].password == creds[1]);
 	    	} else {
 	    		def.resolve(false);
 	    	}
 	    }).catch(function(ex){
-	    	console.log('user', ex);
+	    	console.log('access', ex);
 	    	def.resolve(false);
 	    });
 	  }
     return def.promise;
 	},
-	permission: function(id){
+	username: function(headers) {
     var def = Q.defer();
-
+    var creds = (new Buffer(headers['authorization'].split(' ')[1], 'base64')).toString().split(':');
+    creds = { username: creds[0], password: creds[1] };
+		db.selectOne('user_access', { username: creds.username }).then(function(user){
+			def.resolve(user);
+		}).catch(function(ex){
+    	console.log('username', ex);
+			def.resolve({});
+		});
+    return def.promise;
+	},
+	permission: function(repo, user){
+    var def = Q.defer();
+    // if(RegExp(user.username+"/").exec(repo)) {
+    // 	def.resolve(true);
+    // } else {
+			db.selectOne('url', { url: repo }).then(function(row){
+				console.log(row.repository_id)
+			}).catch(function(ex){
+			  def.resolve(false);
+			  console.log('permission', ex);
+			});
+    // }
     return def.promise;
 	}
 }
