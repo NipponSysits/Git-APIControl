e607324fbee7ed40dfdfa300d090ca48e70e0e40
Kananek T <info.dvgamer@gmail.com>
2016-03-18 14:04:19 +0700

    authorization and control repository

--- libs/auth.js
+++ libs/auth.js
@@ -1,30 +1,41 @@
 var Q = require('q');
-var config = require("./app.config");
 var conn = require("./db");
 var db = conn.connect();
 
 module.exports = {
-	authorization: function(headers){
-    var def = Q.defer(),
+	authorization: function(headers) {
+    var def = Q.defer();
 	  var auth = headers['authorization'];
 	  if (auth === void 0) {
-	    return false;
+	  	def.resolve(false);
 	  } else {
 	    var creds = (new Buffer(auth.split(' ')[1], 'base64')).toString().split(':');
 	    creds = { username: creds[0], password: creds[1] };
 	    db.selectOne('user', { username: creds.username }).then(function(row){
 	    	if(row) {
-	    		def.revolve(row.password === creds.password);
+	    		if(row.password !== creds.password) {
+	    			def.resolve(false);
+	    		} else {
+	    			db.selectOne('user_access', { user_id: row.user_id }).then(function(user){
+	    				def.resolve(user);
+	    			}).catch(function(ex){
+	    				console.log('user_access', ex);
+	    				def.resolve(false);
+	    			});
+	    		}
 	    	} else {
-	    		def.revolve(false);
+	    		def.resolve(false);
 	    	}
 	    }).catch(function(ex){
-	    	def.reject(ex);
+	    	console.log('user', ex);
+	    	def.resolve(false);
 	    });
 	  }
     return def.promise;
 	},
-	permission: function(){
+	permission: function(id){
+    var def = Q.defer();
 
+    return def.promise;
 	}
 }
--- libs/control.js
+++ libs/control.js
@@ -1,5 +1,21 @@
-module.exports = {
-	create: function(req, res){
+var config = require("../app.config");
+var repos = require('pushover')(config.path, { autoCreate: false });
+var Q = require('q');
+var conn = require("./db");
+var db = conn.connect();
 
+module.exports = {
+	create: function(path){
+    var def = Q.defer();
+		repos.exists(path, function(exists){
+			if(!exists) {
+				repos.create(path, function(err){
+					if(err) { def.reject(err); } else { def.resolve(false); }
+				});
+			} else {
+    		def.resolve(true);
+			}
+		})
+    return def.promise;
 	}
 }
