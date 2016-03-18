ead2702c91e9e0b720801c1fe9fe38b30edab4da
KEM <kem@ns.co.th>
2016-03-18 11:16:41 +0700

    auth << authorization

--- libs/auth.js
+++ libs/auth.js
@@ -1,14 +1,30 @@
+var Q = require('q');
+var config = require("./app.config");
+var conn = require("./db");
+var db = conn.connect();
+
 module.exports = {
 	authorization: function(headers){
-	  var auth = req.headers['authorization'];
+    var def = Q.defer(),
+	  var auth = headers['authorization'];
 	  if (auth === void 0) {
 	    return false;
 	  } else {
 	    var creds = (new Buffer(auth.split(' ')[1], 'base64')).toString().split(':');
-	    return { username: creds[0], password: creds[1] };
+	    creds = { username: creds[0], password: creds[1] };
+	    db.selectOne('user', { username: creds.username }).then(function(row){
+	    	if(row) {
+	    		def.revolve(row.password === creds.password);
+	    	} else {
+	    		def.revolve(false);
+	    	}
+	    }).catch(function(ex){
+	    	def.reject(ex);
+	    });
 	  }
+    return def.promise;
 	},
-	permission: function(creds, req, res){
+	permission: function(){
 
 	}
 }
