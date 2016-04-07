d2e0ac59b40347ce8de74d1219fc95b31521aae1
Kananek T <info.dvgamer@gmail.com>
2016-04-07 17:28:52 +0700

    git log
    update command

--- http_git/events/push.js
+++ http_git/events/push.js
@@ -6,20 +6,23 @@ const chalk 		= require('chalk');
 const path 			= require('path');
 
 module.exports = function(push) {
-  push.accept(function(){ 
+  push.accept(function(){
+    var dirRepository = config.path+'/'+push.repo;
+
   	if(push.commit == '0000000000000000000000000000000000000000') {
   		// Delete Branch in Origin
 
   	} else {
-  		
+
   	}
-    var repopath = config.path + '/' + push.repo;
-    var revlist = [ 'rev-list', '--all', '--count' ];
-    var logs = [ '--no-pager','log','--all','--source','--stat','--date=iso','--name-status' ];
+
+    var getTotalList = [ 'rev-list', '--all', '--count' ];
+    var getLogs = [ '--no-pager','log','--all','--source','--stat','--date=iso','--name-status' ];
+    var getComment = [ 'log','-n 1','--format=%B' ]
+    var getGraph = [ '--no-pager', 'log', '--graph', '--oneline', '--since=""' ];
 		var data = { 
 		  commit_index: 22,
 		  repository: push.repo,
-		  parents: null,
 		  commit_id: push.commit,
 		  commit_branch: push.branch,
 		  commit_name: '',
@@ -27,27 +30,22 @@ module.exports = function(push) {
 		  comment_head: '',
 		  comment_full: '',
 		  commit_btn: "Go to commit id '" + push.commit.substr(0, 7) + "'",
-		  commit_link: "//pgm.ns.co.th/"+push.repo+'/info/'+push.commit,
+		  commit_link: "http://pgm.ns.co.th/"+push.repo+'/info/'+push.commit,
 		  domain_name: 'pgm.ns.co.th',
 		  limit_rows: 50,
-		  files: [
-		    { filename: 'dfgnwexwsd1.txt', status: '+', filepath: '/temp/test-mail/node_modules/async/support' },
-		    { filename: 'dfgnwexwsd2.txt', status: '+', filepath: '/temp/test-mail/node_modules/async/tmp' },
-		    { filename: 'dfgnwexwsd2.txt', status: '+', filepath: '/temp/test-mail/node_modules/async/tmp' },
-		    { filename: 'dfgnwexwsd2.txt', status: '+', filepath: '/temp/test-mail/node_modules/async/tmp' }
-		  ]
+		  files: [ { filename: 'file.txt', status: 'A|M|D', filepath: '/' } ]
 		}
 
 		auth.username(push.headers).then(function(user){
 			data.commit_name = user.fullname+'<'+user.email+'>';
     	console.log(user.username, "("+user.fullname+")", 'push /' + push.repo, ':', push.branch);
-    	return control.cmd('git', revlist, repopath);
+    	return control.cmd('git', getTotalList, dirRepository);
     }).then(function(index){
     	data.commit_index = index.replace(/[\n|\r]/g,'');
 
       var since = "-n 1 ";
-			logs.push(since);
-    	return control.cmd('git', logs, repopath);
+			getLogs.push(since);
+    	return control.cmd('git', getLogs, dirRepository);
     }).then(function(log){	
       var sinceFormat = "yyyy-MM-dd HH:mm:ss zzz";
     	var head = /Author:.(.*?>)\W+Date:.(.*)/.exec(log);
