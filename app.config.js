1150e5b9f4efd76818c689045da5198677af042e
Kananek T <info.dvgamer@gmail.com>
2016-04-08 13:04:55 +0700

    local config

--- app.config.js
+++ app.config.js
@@ -4,20 +4,27 @@ module.exports = ({
 		domain: 'localhost',
 		git: 8100,
 		api: 8200,
-		mysql: {
-			host: 'pgm.ns.co.th',
-			user: 'root',
-			port: 3300,
-			password: 'pgm!1234',
-			database: 'js_debugger'
-		},
+		mysql: { host: 'pgm.ns.co.th', user: 'root', port: 3300, password: 'pgm!1234', database: 'js_debugger' },
 		smtp: {
-		  host: '192.168.10.2',
-		  port: 25,
+		  host: '192.168.10.2', port: 25,
 		  auth: { user: 'pgm', pass: '123456' },            
-		  secure: false,
-		  tls: { rejectUnauthorized: false },
-		  debug: true
+		  secure: false, tls: { rejectUnauthorized: false }, debug: true
+		},
+		mongoose: 'mongodb://dev.ns.co.th:20400/debugger',
+		path: './tmp',
+		core: 'C:/Program Files/Git/mingw64/libexec/git-core',
+		lfs: 'C:/Program Files/Git LFS'
+	},
+	local: {
+		run: 'local',
+		domain: '127.0.0.1',
+		git: 8100,
+		api: 8200,
+		mysql: { host: '192.168.26.6', user: 'root', port: 20410, password: '123456', database: 'js_debugger' },
+		smtp: {
+		  host: '192.168.10.2', port: 25,
+		  auth: { user: 'pgm', pass: '123456' },            
+		  secure: false, tls: { rejectUnauthorized: false }, debug: true
 		},
 		mongoose: 'mongodb://dev.ns.co.th:20400/debugger',
 		path: './tmp',
@@ -29,13 +36,7 @@ module.exports = ({
 		domain: 'dev.ns.co.th',
 		git: 810,
 		api: 811,
-		mysql: {
-			host: 'pgm.ns.co.th',
-			user: 'root',
-			port: 3300,
-			password: 'pgm!1234',
-			database: 'js_debugger'
-		},
+		mysql: { host: 'pgm.ns.co.th', user: 'root', port: 3300, password: 'pgm!1234', database: 'js_debugger' },
 		smtp: {
 		  host: '192.168.10.2',
 		  port: 25,
