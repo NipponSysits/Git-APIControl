a1c713859842ed40f3a9bcce5842a93b522d26f0
Kananek T <info.dvgamer@gmail.com>
2016-04-11 21:53:12 +0700

    fixed permission

--- http_git/events/info.js
+++ http_git/events/info.js
@@ -5,20 +5,20 @@ const chalk   = require('chalk');
 module.exports = function(info) {
   auth.username(info.headers).then(function(user){
     // console.log('info check', user.level);
-    if(user.level > 0) {
-      auth.permission(info.repo, user).then(function(accept){
-        // console.log('accept', accept);
-        if(accept) {
-          console.log(user.username, "("+user.fullname+")",'info /'+info.repo);
-          info.accept();
-        } else {
-          console.log(chalk.red('reject--'+info.headers,'/'+info.repo));
-          info.reject('Please check username and password is not currect');
-        }
-      });
-    } else {
-      console.log(user.username, "("+user.fullname+")",'info /'+info.repo);
-      info.accept();
-    }
+    // if(user.level > 0) {
+    auth.permission(info.repo, user).then(function(permission){
+      if(permission.accept) {
+        console.log(user.username, "("+user.fullname+")",'info /'+info.repo);
+        info.accept();
+      } else {
+        console.log(chalk.red('reject--'+info.repo));
+        console.log(chalk.red('message--'+permission.error));
+        info.reject();//'Please check username and password is not currect'
+      }
+    });
+    // } else {
+    //   console.log(user.username, "("+user.fullname+")",'info /'+info.repo);
+    //   info.accept();
+    // }
   });
 }
