eaca35f5285e0a02c78c15ef5b154b25db2e22c4
Kananek T <info.dvgamer@gmail.com>
2016-04-18 16:46:10 +0700

    fixed

--- http_git/events/fetch.js
+++ http_git/events/fetch.js
@@ -1,10 +1,10 @@
 const auth 		= require(".custom/touno-git").auth;
 
 module.exports = function(fetch) {
-	auth.username(fetch.headers).then(function(user){
-	  console.log(user.username, "("+user.fullname+")", chalk.blue('fetch /'+info.repo));
+	// auth.username(fetch.headers).then(function(user){
+	//   console.log(user.username, "("+user.fullname+")", chalk.blue('fetch /'+info.repo));
 	  fetch.accept();
-	}).catch(function(){
-		fetch.reject();
-	});
+	// }).catch(function(){
+	// 	fetch.reject();
+	// });
 }
