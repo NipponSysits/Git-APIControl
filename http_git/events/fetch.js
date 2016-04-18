1d9e679d26c2450e496aa91d2d947f6b1c7345b2
Kananek T <info.dvgamer@gmail.com>
2016-04-18 16:58:39 +0700

    error: fatal: RPC failed; HTTP 500 curl 22
    The requested URL returned error: 500 Internal Server Error

--- http_git/events/fetch.js
+++ http_git/events/fetch.js
@@ -1,10 +1,11 @@
 const auth 		= require(".custom/touno-git").auth;
+const chalk   = require('chalk');
 
 module.exports = function(fetch) {
 	auth.username(fetch.headers).then(function(user){
-	  console.log(user.username, "("+user.fullname+")", chalk.blue('fetch /'+info.repo));
+	  console.log(user.username, "("+user.fullname+")", chalk.blue('fetch /'+fetch.repo));
 	  fetch.accept();
-	}).catch(function(){
+	}).catch(function(ex){
 		fetch.reject();
 	});
 }
