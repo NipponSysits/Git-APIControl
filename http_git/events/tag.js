b461617a475a79c396749f5d8802353deaf7f51f
Kananek T <info.dvgamer@gmail.com>
2016-05-04 09:26:06 +0700

    fixed : tag vars

--- http_git/events/tag.js
+++ http_git/events/tag.js
@@ -4,7 +4,7 @@ const chalk   	= require('chalk');
 module.exports = function(tag) {
   var infoTime = moment().format('HH:mm:ss, dddd');
   if(typeof tag.commit != 'undefined') {
-	  console.log(chalk.yellow(infoTime), 'tag ' + head.repo);
+	  console.log(chalk.yellow(infoTime), 'tag ' + tag.repo);
   }
   tag.accept();
 }
