060fa035e7aa11c99aa4c55348ce3c89e468745e
Kananek T <info.dvgamer@gmail.com>
2016-05-06 02:27:17 +0700

    change custom config

--- test/emails.js
+++ test/emails.js
@@ -14,20 +14,20 @@ const path = require('path');
 const EmailTemplate = require('email-templates').EmailTemplate;
 
 let options = {
-
+  to: [ 'kem@ns.co.th'],
 }
 
 let data = { 
   commit_index: 22,
   repository: '/dvgamer/test.git',
-  commit_id: '00000000000000000000000000000000',
+  commit_id: 'asdasdasdasdasdasdasdasdasd',
   commit_branch: 'master',
   commit_name: '',
   commit_date: '',
   comment_subject: '',
   comment_full: '',
   commit_btn: "Go to commit id '0000000'",
-  commit_link: "http://pgm.ns.co.th/Source/dvgamer/test.git/info/00000000000000000000000000000000",
+  commit_link: "http://pgm.ns.co.th/Source/dvgamer/test.git/info/asdasdasdasdasdasdasdasdasd",
   domain_name: 'pgm.ns.co.th',
   limit_rows: 50,
   avatar: '',
@@ -40,37 +40,47 @@ let data = {
 }
 
 let req = {
-  headers: [],
-  repo: ''
+  headers: { 
+      host: 'code.touno-k.com',
+      authorization: 'Basic ZHZnYW1lcjpkdmc3cG84YWk=',
+      'user-agent': 'git/2.8.1.windows.1',
+      accept: '*/*',
+      'accept-encoding': 'gzip',
+      'accept-language': 'en-US, *;q=0.9',
+      pragma: 'no-cache' 
+  },
+  repo: 'check-stock/APIstock_TCRadio.git'
 }
 
 
-describe('function email-templates()', function() {
-  it('templates -- changeset-branch', function() {
+describe('function notification email()', function() {
+  it('templates -- changeset-branch', function(done) {
     return git.email('changeset-branch', options, data, req).then(function(result){
+      done();
       expect(result).to.equal(undefined);
     });
     // var cartSummary = new CartSummary([]);
   });
 
-  it('templates -- changeset-email', function() {
+  it('templates -- changeset-email', function(done) {
     return git.email('changeset-email', options, data, req).then(function(result){
+      done();
       expect(result).to.equal(undefined);
     });
     // var cartSummary = new CartSummary([]);
   });
 
-  it('templates -- changeset-logs', function() {
-    return git.email('changeset-logs', options, data, req).then(function(result){
-      expect(result).to.equal(undefined);
-    });
-    // var cartSummary = new CartSummary([]);
-  });
+  // it('templates -- changeset-logs', function() {
+  //   return git.email('changeset-logs', options, data, req).then(function(result){
+  //     expect(result).to.equal(undefined);
+  //   });
+  //   // var cartSummary = new CartSummary([]);
+  // });
 
-  it('templates -- forgot-password', function() {
-    return git.email('forgot-password', options, data, req).then(function(result){
-      expect(result).to.equal(undefined);
-    });
-    // var cartSummary = new CartSummary([]);
-  });
+  // it('templates -- forgot-password', function() {
+  //   return git.email('forgot-password', options, data, req).then(function(result){
+  //     expect(result).to.equal(undefined);
+  //   });
+  //   // var cartSummary = new CartSummary([]);
+  // });
 });
