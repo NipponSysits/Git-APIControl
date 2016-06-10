1d012f8f9d09850232719c332dc4774b7f2479b2
Kananek T <info.dvgamer@gmail.com>
2016-06-10 14:00:37 +0700

    tag logs commit

--- http_git/events/tag.js
+++ http_git/events/tag.js
@@ -8,6 +8,18 @@ module.exports = function(tag) {
   var infoTime = moment().format('HH:mm:ss, dddd');
   auth.username(tag.headers).then(function(user){
 	  if(typeof tag.commit != 'undefined') {
+			// let log = new mongo.History({
+			// 	commit_id: push.commit,
+			// 	repository_id: access.repository_id,
+			// 	author: access.fullname, 
+			// 	email: access.email, 
+			// 	subject: `removed your branch is '${push.branch}'`, 
+			// 	comment: null, 
+			// 	logs: false,
+			// 	since: new Date(),
+			// });
+			// log.save(function (err, log) { if (err) def.reject(err);	});
+			
 	  	console.log(chalk.yellow(infoTime), "tags", user.fullname, "info", repo);
 	  }
   	tag.accept();
