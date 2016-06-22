4f9b793f51eb5f0eea1d37956765322c3d7d7c3d
Kananek T <info.dvgamer@gmail.com>
2016-06-23 01:23:58 +0700

    add branch API and change response cmd

--- cmd/list-files.cmd
+++ cmd/list-files.cmd
@@ -1,4 +1,4 @@
 @echo off
-FOR /F "tokens=*" %%D IN ('git ls-tree --name-only %1') DO (
+FOR /F "tokens=*" %%D IN ('git ls-tree -l %1') DO (
   git log -1 --format="%%D|%%ci|%%s" -- %%D
 )
--- http_api/repository/setting.js
+++ http_api/repository/setting.js
@@ -11,12 +11,25 @@ router.get('/create', function(req, res){
   res.end('create');
 });
 
+
+
+
 router.get('/delete', function(req, res){
-  res.end('delete');
+  // git show master:readme.md
 });
 
-router.get('/update', function(req, res){
-  res.end('update');
+router.get('/branch/:collection/:repository', function(req, res){
+  let param = req.params, dir_name = '';
+  db.query(sql, { dir: `${param.collection}/${param.repository}` }).then(function(data){
+    dir_name = `${config[data[0].config]}/${param.collection}/${param.repository}`;
+    return control.cmd('git', [ 'show-branch','--list' ], dir_name)
+  }).then(function(branch){
+    res.end(JSON.stringify([]));
+  }).catch(function(err){
+    console.log('repository:', dir_name);
+    console.log('branch--catch', err);
+    res.end('[]');
+  });
 });
 
 router.get('/files/:collection/:repository/:branch', function(req, res){
@@ -26,36 +39,28 @@ router.get('/files/:collection/:repository/:branch', function(req, res){
   db.query(sql, { dir: `${param.collection}/${param.repository}` }).then(function(data){
     dir_name = `${config[data[0].config]}/${param.collection}/${param.repository}`;
   }).then(function(){
-    	// LINUX  :: git ls-tree --name-only master | while read filename; do (echo "$filename|$(git log -1 --format="%ci|%s" -- $filename)") done
-    	// WINDOW :: 
-
-			// git ls-tree --full-tree master
-			// git show-branch --list
-
-			// git show master:readme.md
     if(config.platfrom === 'LINUX') {
-      return control.cmd('git', [ 'ls-tree','--name-only',param.branch, '| while read filename; do (echo "$filename|$(git log -1 --format="%ci|%s" -- $filename)") done' ], dir_name);
+      return control.cmd('git', [ 'ls-tree','-l',param.branch, '| while read filename; do (echo "$filename|$(git log -1 --format="%ci|%s" -- $filename)") done' ], dir_name);
     } else {
       return control.cmd('cmd', [ '/c', '..\\..\\..\\..\\cmd\\list-files',param.branch ], dir_name);
     }
   }).then(function(git){
     git.match(/(.*)\|(.*)\|(.*)\n/ig).forEach(function(item){
-      let logs = /(.*)\|(.*)\|(.*)/ig.exec(item);
-      let file = {
-        ext: (/\.[\w\d]+$/gm.exec(logs[1]) || [])[0] || null,
-        filename: logs[1],
-        since: logs[2],
-        comment: logs[3].replace(/\n/g, '')
-      }
-
-      files.push(file);
+      let logs = /\d{6}(.+)[a-f0-9]{40}([\s\d\-]+)(.*)\|(.*)\|(.*)/ig.exec(item);
+      files.push({
+        ext: logs[2].trim() === '-' ? null : (/\.[\w\d]+$/gm.exec(logs[3]) || [])[0] || null,
+        size: logs[2].trim() === '-' ? 0 : parseInt(logs[2].trim()),
+        filename: logs[3],
+        since: logs[4],
+        comment: logs[5].replace(/\n/g, '')
+      });
     });
     res.end(JSON.stringify(files));
   }).catch(function(err){
     console.log('repository:', dir_name);
     console.log('file--catch', err);
     res.end('[]');
-   });
+  });
 
 });
 
